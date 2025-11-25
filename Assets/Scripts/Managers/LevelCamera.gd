9e0ca3bad5d61c506f0a3c9f9f55adf357c4744b
Tlotro <misha.pletneow@yandex.ru>
2025-11-25 20:58:17 +0300

    Adjust camera angel, fix zoom, scale camera move speed to zoom.

--- Assets/Scripts/Managers/LevelCamera.gd
+++ Assets/Scripts/Managers/LevelCamera.gd
@@ -10,24 +10,28 @@ func _ready() -> void:
 	projection = Camera3D.PROJECTION_ORTHOGONAL
 
 func _process(delta):
-	var input_vector = Vector2(0, 0)
+	var input_vector = Vector3.ZERO
 	
 	if Input.is_key_pressed(KEY_D):
 		input_vector.x += 1
 	if Input.is_key_pressed(KEY_A):
 		input_vector.x -= 1
 	if Input.is_key_pressed(KEY_S):
-		input_vector.y += 1
+		input_vector.z += 1
 	if Input.is_key_pressed(KEY_W):
-		input_vector.y -= 1
+		input_vector.z -= 1
 
-	input_vector = input_vector.normalized()
-	position += Vector3(input_vector.x, 0, input_vector.y) * move_speed * delta
+	input_vector = input_vector.normalized().rotated(Vector3.UP,rotation.y)
+	position += input_vector * move_speed * delta * size/30
 
 func _input(event):
 	if event is InputEventMouseButton:
 		match event.button_index:
 			MOUSE_BUTTON_WHEEL_UP:
-				size = clamp(size - zoom_speed, size_min, size_max)
+				var size_change = size - clamp(size - zoom_speed, size_min, size_max)
+				position += (Vector3.FORWARD * size_change).rotated(Vector3.RIGHT,rotation.x).rotated(Vector3.UP,rotation.y)
+				size -= size_change
 			MOUSE_BUTTON_WHEEL_DOWN:
-				size = clamp(size + zoom_speed, size_min, size_max)
+				var size_change = clamp(size + zoom_speed, size_min, size_max) - size
+				position -= (Vector3.FORWARD * size_change).rotated(Vector3.RIGHT,rotation.x).rotated(Vector3.UP,rotation.y)
+				size += size_change
